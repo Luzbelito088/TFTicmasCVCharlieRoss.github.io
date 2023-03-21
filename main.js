0a62d51abf653ca74a82b48961177ae4b0d1b2b9
Facundo Alvarez <123787145+Luzbelito088@users.noreply.github.com>
2023-03-20 22:11:08 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+document.getElementById('boton1').onclick = function Experiencia (){
+  console.log ("click 1");
+  document.getElementById('ExperienciaSub').innerHTML = "ahora es el mañana.";
+}
+
+document.getElementById('boton2').onclick = function Educacion (){
+  console.log ("click 2");
+  document.getElementById('SubtituloEdu').innerHTML = "es por lo que aprendiste.";
+}
+
+document.getElementById('boton3').onclick = function Contacto (){
+  console.log ("click 3");
+  document.getElementById('SubContacto').innerHTML = "hay un solo paso: Contactame.";
+}
+
+// intenté hacer un codigo para el formulario, pero me está costando por lo que mejor no me arriesgo hasta aprenderlo bien del todo
